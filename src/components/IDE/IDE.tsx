d17aecac49b78a441fc50bfd4246d06bb09986c8
ronen.albagli@opora.io <ronen.albagli@opora.io>
2020-01-25 20:04:53 +0200

    add ace editor

---
+++ src/components/IDE/IDE.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+
+const IDE = (props: any) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        right: "25px",
+        width: "45%",
+        height: "800px"
+      }}
+    >
+      <div>
+        <AceEditor
+          mode="javascript"
+          theme="monokai"
+          //   onChange={this.onChange}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{
+            $blockScrolling: true,
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true
+          }}
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          //   value={`${funcName}${logicCode} \n }`}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 2
+          }}
+          style={{
+            backgroundColor: "#0000001a",
+            width: "100%",
+            height: "800px"
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default IDE;
--- src/screens/new-battle/newBattle.tsx
+++ src/screens/new-battle/newBattle.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FormBattle from "../../components/forms/battleForm";
+import IDE from "../../components/IDE/IDE";
 
 const NewBattle: React.FC = (props: any) => {
   return (
-    <div>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
       <FormBattle />
+      <IDE />
     </div>
   );
 };
