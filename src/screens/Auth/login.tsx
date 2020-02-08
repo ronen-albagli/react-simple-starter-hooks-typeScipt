9b3a47705ee7cf188dc8d9c8bba1df94f3396d1d
ronen.albagli@opora.io <ronen.albagli@opora.io>
2020-02-08 23:42:07 +0200

    integrate with fb, google in work

--- src/screens/Auth/login.tsx
+++ src/screens/Auth/login.tsx
@@ -6,6 +6,10 @@ import Button from "../../components/buttons/Button";
 import fb from "../../assets/icons/facebook-logo.png";
 import gl from "../../assets/icons/search.png";
 
+import FacebookLogin from "react-facebook-login";
+
+import GoogleLogin from "react-google-login";
+
 const AppLogin = (props: any) => {
   const [loginForm, setLoginForm] = useState({
     email: "",
@@ -19,6 +23,14 @@ const AppLogin = (props: any) => {
     (newState as any)[`${event.target.name}`] = event.target.value;
     setLoginForm({ ...newState });
   };
+
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+  const responseFacebook = (response: any) => {
+    console.log(response);
+  };
+
   return (
     <AppModal show={props.open} close={props.closeModal}>
       <div className="header">
@@ -73,8 +85,23 @@ const AppLogin = (props: any) => {
               alt=""
               src={fb}
             />
+            <div className="fb hidden">
+              <FacebookLogin
+                appId="539926940215177" //APP ID NOT CREATED YET
+                fields="name,email,picture"
+                callback={responseFacebook}
+              />
+            </div>
           </div>
           <div>
+            <div className="gl hidden">
+              <GoogleLogin
+                clientId="40990008904-4l7j8fd6tulggksmfu2ghll2lk5mtfid.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+                buttonText="LOGIN WITH GOOGLE"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+              />
+            </div>
             <img
               style={{
                 height: props.height ? props.height : "20px",
@@ -85,6 +112,18 @@ const AppLogin = (props: any) => {
             />
           </div>
         </div>
+        {/* <GoogleLogin
+          clientId="40990008904-4l7j8fd6tulggksmfu2ghll2lk5mtfid.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+          buttonText="LOGIN WITH GOOGLE"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
+
+        <FacebookLogin
+          appId="539926940215177" //APP ID NOT CREATED YET
+          fields="name,email,picture"
+          callback={responseFacebook}
+        /> */}
       </div>
     </AppModal>
   );
